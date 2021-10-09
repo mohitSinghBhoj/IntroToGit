1f026be4a31f79ba194833b14a9089573602efe0
Mohit Bhoj <amanbhoj2001@outlook.com>
2021-10-09 12:31:52 +0530

    basic commands of git

--- learn.js
+++ learn.js
@@ -1,1 +1,6 @@
 console.log("learning git");
+console.log("git status to chechk the status of repo");
+console.log("git log to check commit history");
+console.leg("git add to add a new file to repo");
+console.log("git diff to see the difference bw the previous and current code of file");
+
