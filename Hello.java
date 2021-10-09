78da8cf6517bf9517678688e30e59cb952b1b237
Mohit Bhoj <amanbhoj2001@outlook.com>
2021-10-09 12:41:45 +0530

    NameByCharacter

--- Hello.java
+++ Hello.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 public class Hello{
 	public static void main(String[] args){
 		String s=sc.nextLine();
-		greetMe(s);
+		PrintMe(s);
 	}
-	public static greetMe(String name){
-		System.out.println("HELLO"+" "+name);
+	public static PrintMe(String name){
+		for(char c:name){
+			System.out.print(c+" ");
+	
+		}
 	}
-
+}
