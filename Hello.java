b16f7bab92fe5d8a9f5e9d7fcb478a9d3f98ef4c
Mohit Bhoj <amanbhoj2001@outlook.com>
2021-10-09 12:20:34 +0530

    greet by name

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Hello{
+	public static void main(String[] args){
+		String s=sc.nextLine();
+		greetMe(s);
+	}
+	public static greetMe(String name){
+		System.out.println("HELLO"+" "+name);
+	}
+
